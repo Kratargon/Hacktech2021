c9e9d08f1f59e37c4cde93ac6a22f0511faf0927
Kratargon <lmdavis@caltech.edu>
2021-04-17 19:36:53 -0700

    added stuff

--- Board/main.py
+++ Board/main.py
@@ -3,10 +3,19 @@ import pygame
 from Board import Board
 import Pieces
 
-
 def gen_pawns():
     for i in range(board.size):
-        Pieces.Pawn(board, board.get_square(2, i), True)
+        Pieces.Pawn(board, board.get_square(1, i), True)
 
     for i in range(board.size):
         Pieces.Pawn(board, board.get_square(board.size - 2, i), False)
+
+def gen_bishops():
+    Pieces.Bishop(board, board.get_square(0, board.size - 3), True)
+    Pieces.Bishop(board, board.get_square(0, 3), True)
+    Pieces.Bishop(board, board.get_square(board.size - 1, board.size - 3), False)
+    Pieces.Bishop(board, board.get_square(board.size - 1, 3), False)
+
+def gen_knights():
+    Pieces.Bishop(board, board.get_square(0, board.size - 3))
+    Pieces.Bishop(board, board.get_square(0, 3))

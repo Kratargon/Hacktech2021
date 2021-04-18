2576017a38999bebabb0735cf0a081ec53278ec0
Kratargon <lmdavis@caltech.edu>
2021-04-17 18:47:05 -0700

    Update Rook.py

--- Pieces/Rook.py
+++ Pieces/Rook.py
@@ -5,56 +5,24 @@ from Pieces import Piece
 class Rook(Piece.Piece):
     def __init__(self, board, square, color):
         super().__init__(board, square, color, "rook")
-        self.bonuses = {Bonus.Knighted(): True, Bonus.Diagonal(): False}
+        self.bonuses = {Bonus.Knighted: False, Bonus.Diagonal: False}
 
     def generate_moves(self):
-        moves = []
+        self.moves = []
 
-        # down
-        for i in range(self.game_pos[1], self.board.size, 1):
-            newpos = self.game_pos[0], i
-            if self.board.check_bounds(newpos):
-                if newpos != self.game_pos:
-                    if self.board.get_square(newpos).has_piece:
-                        if self.can_capture(newpos):
-                            moves.append(newpos)
-                        break
-                    moves.append(newpos)
-
-        # up
-        for i in range(self.game_pos[1], -1, -1):
-            newpos = self.game_pos[0], i
-
-            if self.board.check_bounds(newpos):
-                if newpos != self.game_pos:
-                    if self.board.get_square(newpos).has_piece:
-                        if self.can_capture(newpos):
-                            moves.append(newpos)
-                        break
-                    moves.append(newpos)
-
-        # right
-        for i in range(self.game_pos[0], self.board.size, 1):
-            newpos = i, self.game_pos[1]
-
-            if self.board.check_bounds(newpos):
-                if newpos != self.game_pos:
-                    if self.board.get_square(newpos).has_piece:
-                        if self.can_capture(newpos):
-                            moves.append(newpos)
-                        break
-                    moves.append(newpos)
-
-        # left
-        for i in range(self.game_pos[0], -1, -1):
-            newpos = i, self.game_pos[1]
-
-            if self.board.check_bounds(newpos):
-                if newpos != self.game_pos:
-                    if self.board.get_square(newpos).has_piece:
-                        if self.can_capture(newpos):
-                            moves.append(newpos)
-                        break
-                    moves.append(newpos)
+        self.smolFunction(0, 1)
+        self.smolFunction(0, -1)
+        self.smolFunction(1, 0)
+        self.smolFunction(-1, 0)
 
         return moves
+
+    def smolFunction(self, x, y):
+        for i in range(1, self.board.size):
+            newpos = add(self.game_pos, (i * x, i * y))
+            if self.board.check_bounds(newpos):
+                if self.board.get_square(newpos).has_piece and newpos != self.game_pos:
+                    if self.can_capture(newpos):
+                        self.moves.append(newpos)
+                    break
+                self.moves.append(newpos)
